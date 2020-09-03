002a9a4003f28fb21afb23a5545ab45b7b783e6a
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-09-03 10:28:21 +0900

    物理名変更(chatテーブル・taskテーブル)

--- db/db-challenge2/dbchallenge2.sql
+++ db/db-challenge2/dbchallenge2.sql
@@ -17,7 +17,7 @@ CREATE TABLE chat (
   id                         INTEGER PRIMARY KEY AUTO_INCREMENT,
   name                       VARCHAR(100) NOT NULL,
   overview                   VARCHAR(1000),
-  is_permission_send_file    TINYINT(1) DEFAULT 1 NOT NULL,
+  is_file_sendable           TINYINT(1) DEFAULT 1 NOT NULL,
   is_direct_chat             TINYINT(1) DEFAULT 0 NOT NULL,
   is_deleted                 TINYINT(1) DEFAULT 0 NOT NULL,
   create_user_id             INTEGER NOT NULL REFERENCES user(id),
@@ -41,17 +41,17 @@ CREATE TABLE post (
 
 -- taskテーブル
 CREATE TABLE task (
-  id             INTEGER PRIMARY KEY AUTO_INCREMENT,
-  content        VARCHAR(1000) NOT NULL,
-  main_user_id   INTEGER NOT NULL REFERENCES user(id),
-  chat_id        INTEGER NOT NULL REFERENCES chat(id),
-  dead_line      DATETIME,
-  is_completed   TINYINT(1) DEFAULT 0 NOT NULL,
-  is_deleted     TINYINT(1) DEFAULT 0 NOT NULL,
-  create_user_id INTEGER NOT NULL REFERENCES user(id),
-  created_at     DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  update_user_id INTEGER NOT NULL REFERENCES user(id),
-  updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+  id                 INTEGER PRIMARY KEY AUTO_INCREMENT,
+  content            VARCHAR(1000) NOT NULL,
+  assigned_user_id   INTEGER NOT NULL REFERENCES user(id),
+  chat_id            INTEGER NOT NULL REFERENCES chat(id),
+  dead_line          DATETIME,
+  is_completed       TINYINT(1) DEFAULT 0 NOT NULL,
+  is_deleted         TINYINT(1) DEFAULT 0 NOT NULL,
+  create_user_id     INTEGER NOT NULL REFERENCES user(id),
+  created_at         DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  update_user_id     INTEGER NOT NULL REFERENCES user(id),
+  updated_at         TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 ) ENGINE = INNODB;
 
 -- chat_participantテーブル
