0ed6c3a00cd0109105a1d7849a4513d7db399dba
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-09-06 11:23:29 +0900

    db-013　空文字をUPDATE対象に追加

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -1,7 +1,7 @@
 BEGIN;
 UPDATE user
    SET is_deleted = 1
- WHERE businessphone_number IS NULL
-   AND cellphone_number IS NULL
+ WHERE (businessphone_number IS NULL OR businessphone_number = '')
+   AND (cellphone_number IS NULL OR cellphone_number = '')
    AND is_deleted <> 1;
 COMMIT;
